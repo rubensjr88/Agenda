62f0ac6c7b10b5dc54fa556f714adbec0cab3e84
rubensjr88 <rubensjr88@gmail.com>
2018-08-11 14:52:39 -0400

    Iniciando projeto no github

---
+++ app/src/main/java/br/com/rubensoliveira/agenda/ListaAlunosActivity.java
@@ -0,0 +1,21 @@
+package br.com.rubensoliveira.agenda;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_alunos);
+
+        String[] alunos = {"Rubens", "Ana", "Valentina", "Paulecir", "Aláric", "Déborah", "Rachel", "Roger", "Pietra", "Bernardo", "Ozzy", "Kim", "Thor"};
+        ListView listaAlunos = (ListView) findViewById(R.id.lista_alunos);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, alunos);
+        listaAlunos.setAdapter(adapter);
+
+    }
+}
