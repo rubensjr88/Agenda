a3640fd9135608f30ece4b837c2fc82543c465a4
rubensjr88 <rubensjr88@gmail.com>
2018-08-18 15:07:40 -0400

    Iniciando projeto agenda - Alura

--- app/src/main/java/br/com/rubensoliveira/agenda/FormularioActivity.java
+++ app/src/main/java/br/com/rubensoliveira/agenda/FormularioActivity.java
@@ -2,6 +2,10 @@ package br.com.rubensoliveira.agenda;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FormularioActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FormularioActivity.this, "Aluno salvo", Toast.LENGTH_SHORT).show();
+
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
